b212d5cf68b729ef96276628f037317bc2eda9b5
Mohammad Faisal <mohammadfaisal1011@gmail.com>
2022-05-29 00:45:48 +0600

    FIxed base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     domains: [
       "images.unsplash.com",
